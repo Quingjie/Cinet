cb7cae1f68a0f1e38ec3531c6f55e4b9be56d3ba
Katell <katell.gouzerh@hotmail.com>
2024-12-16 00:45:42 +0100

    sans secret

--- cinetica/components/ui/ShowCarousel.tsx
+++ cinetica/components/ui/ShowCarousel.tsx
@@ -1,4 +1,3 @@
-//components/ui/ShowCarousel.tsx
 import * as React from "react";
 import ShowCard from "./ShowCard";
 import {
@@ -17,39 +16,34 @@ interface ShowCarouselProps {
 
 const ShowCarousel: React.FC<ShowCarouselProps> = ({ shows, pageMode }) => {
   return (
-    <Carousel 
-      className="w-full max-w-7xl" 
-      opts={{
-        align: "start",
-        loop: true,
-      }}
-    >
-      <CarouselContent>
-        {shows.map((show) => (
-          <CarouselItem 
-            key={show.id}
-            className="
-              basis-full 
-              sm:basis-1/2 
-              md:basis-1/3 
-              lg:basis-1/4 
-              xl:basis-1/5 
-              2xl:basis-1/6 
-              p-2
-            "
-          >
-            <ShowCard
-              show={show}
-              theme={pageMode === "primary" ? "light" : "dark"}
-              size="small"
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="w-full max-w-7xl mx-auto relative">
+      <Carousel 
+        className="w-full" 
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {shows.map((show) => (
+            <CarouselItem 
+              key={show.id}
+              className="min-w-0 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5 2xl:basis-1/6 p-2"
+            >
+              <ShowCard
+                show={show}
+                theme={pageMode === "primary" ? "light" : "dark"}
+                size="small"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute left-0 top-1/2 -translate-y-1/2 hidden sm:flex" />
+        <CarouselNext className="absolute right-0 top-1/2 -translate-y-1/2 hidden sm:flex" />
+      </Carousel>
+    </div>
   );
 };
 
 export default ShowCarousel;
+
