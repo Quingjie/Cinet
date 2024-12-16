839abfc58c95927b6af66bc464b58954415d8c6a
Katell <katell.gouzerh@hotmail.com>
2024-12-16 03:24:58 +0100

    auth

--- cinetica/app/api/movie/top-rated/route.ts
+++ cinetica/app/api/movie/top-rated/route.ts
@@ -1,7 +1,7 @@
 import { Movie } from '../../../entities/movie';
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth";
-import { authOptions } from '../../auth/[...nextauth]/route';
+import { authOptions } from '../../auth/route';
 
 export async function GET() {
   try {
